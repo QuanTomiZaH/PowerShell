8a6bccb8293bbec288bf1f9ffc4200e92191439d
adm-rgjaltema <robin.gjaltema@enexis.nl>
2020-10-12 11:48:43 +0200

    Added bulk creation AD groups script + example CSV

---
+++ Active Directory/Create Bulk groups/Create_Bulk_Groups.ps1
@@ -0,0 +1,35 @@
+# Robin Gjaltema 2020-10
+#
+#
+#
+Import-Module ActiveDirectory
+#Import CSV
+$path = Split-Path -parent $MyInvocation.MyCommand.Definition
+$newpath = $path + "\bulk_input.csv"
+$csv = @()
+$csv = Import-Csv -Path $newpath
+
+#Get Domain Base
+$searchbase = Get-ADDomain | ForEach { $_.DistinguishedName }
+
+#Loop through all items in the CSV
+ForEach ($item In $csv) {
+  #Check if the OU exists
+  $check = [ADSI]::Exists("LDAP://$($item.GroupLocation),$($searchbase)")
+
+  If ($check -eq $True) {
+    Try {
+      #Check if the Group already exists
+      $exists = Get-ADGroup $item.GroupName
+      Write-Host "Group $($item.GroupName) alread exists! Group creation skipped!"
+    }
+    Catch {
+      #Create the group if it doesn't exist
+      $create = New-ADGroup -Name $item.GroupName -GroupScope $item.GroupType -Description $item.Description -Path ($($item.GroupLocation) + "," + $($searchbase))
+      Write-Host "Group $($item.GroupName) created!"
+    }
+  }
+  Else {
+    Write-Host "Target OU can't be found! Group creation skipped!"
+  }
+}
